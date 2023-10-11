11aa14ae0ba8f62a680ed7b83f7e112d66a20d86
maged elgndy <maged.elgndy999@gmail.com>
2023-10-11 12:31:22 +0300

    Add project files.

---
+++ PublisherDomain/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublisherDomain
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public DateTime PublishDate { get; set; }
+        public Decimal BasePrice { get; set; }
+
+        public Author Author { get; set; }
+        public int AuthorId { get; set; }   
+    }
+}
+
+
+
+
